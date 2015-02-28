c6988692ddd4472730bd6549a11b69075b222b9f
CChabert <chabert.caroline@gmail.com>
2015-02-28 20:07:39 +0100

    Récupération DataServer

---
+++ src/objectsTemplates/Pompiers.java
@@ -0,0 +1,96 @@
+package objectsTemplates;
+
+import java.util.List;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/*
+ * Created with Eclipse.
+ * Author : Caroline Chabert
+ * Class to get and set firemans data hosted by the server (cloning of data)
+ * 
+ */
+
+@XmlRootElement
+public class Pompiers {
+	
+	private int id;
+	private String mdp;
+	private boolean directeur;
+	private String nom;
+	private String prenom;
+	private List<String> UV;
+	private List<String> enCours;
+	private List<String> accepte;
+	private List<String> attente;
+	private List<String> refuse;
+	private List<String> gestion;
+	
+	/*
+	 * Id of the fireman
+	 */
+	public void setId(int id) { this.id = id; }
+	public int getId(){ return id; }
+	
+	/*
+	 * Mdp of the fireman
+	 */
+	public void setMdp(String mdp) { this.mdp = mdp; }
+	public String getMdp(){ return mdp; }
+	
+	/*
+	 * Is this fireman a director ?
+	 */
+	public void setDirecteur(String directeur) { 
+		this.directeur = (directeur.equals("oui")); 
+	}
+	public boolean getDirecteur(){ return directeur; }
+	
+	/*
+	 * Last name of the fireman
+	 */
+	public void setNom(String nom) { this.nom = nom; }
+	public String getNom(){return nom;}
+	
+	/*
+	 * First name of the fireman
+	 */
+	public void setPrenom(String prenom) { this.prenom = prenom; }
+	public String getPrenom(){ return prenom; }
+	
+	/*
+	 * UV suggested to the fireman or complete UV list which fireman candidated ??
+	 */
+	public void setUV(List<String> UV) { this.UV = UV; }
+	public List<String> getUV(){ return UV; }
+	
+	/*
+	 * Current UV session
+	 */
+	public void setEnCours(List<String> enCours) { this.enCours = enCours; }
+	public List<String> getEnCours(){ return enCours; }
+	
+	/*
+	 * Accepted future UV session
+	 */
+	public void setAccepte(List<String> accepte) { this.accepte = accepte; }
+	public List<String> getAccepte(){return accepte;}
+	
+	/*
+	 * Pending future UV session
+	 */
+	public void setAttente(List<String> attente) { this.attente = attente; }
+	public List<String> getAttente(){ return attente; }
+	
+	/*
+	 * Refused future UV session
+	 */
+	public void setRefuse(List<String> refuse) { this.refuse = refuse; }
+	public List<String> getRefuse(){ return refuse; }
+	
+	/*
+	 * Management of the UV session by the director
+	 */
+	public void setGestion(List<String> gestion) { this.gestion = gestion; }
+	public List<String> getGestion(){ return gestion; }
+
+}
---
+++ src/objectsTemplates/Stage.java
@@ -0,0 +1,87 @@
+package objectsTemplates;
+
+import java.util.Calendar;
+import java.util.List;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/*
+ * Created with Eclipse.
+ * Author : Caroline Chabert
+ * Class to get and set sessions data hosted by the server (cloning of data)
+ * 
+ */
+
+@XmlRootElement
+public class Stage {
+
+	private int directeur;
+	private String uv;
+	private Calendar date;
+	private String lieu;
+	private String infos;
+	private List<String> candidats;
+	private List<String> accepte;
+	private List<String> attente;
+	private List<String> refuse;
+	
+	/*
+	 * ID director of the session
+	 */
+	public void setDirecteur(int directeur) { this.directeur = directeur; }
+	public int getDirecteur() { return directeur; }
+	
+	/*
+	 * UV of the session
+	 */
+	public void setUV(String uv) { this.uv = uv; }
+	public String getUV() { return uv; }
+	
+	/*
+	 * Date of the session
+	 */
+	public void setDate(Calendar date) { this.date = date; }
+	public String getDate() {
+		 String currentDate = date.get(Calendar.DAY_OF_MONTH) +
+				 				"/" + date.get(Calendar.MONTH) +
+				 				"/" + date.get(Calendar.YEAR);
+		 return currentDate;
+	}
+	
+	/*
+	 * Place of the session
+	 */
+	public void setLieu(String lieu) { this.lieu = lieu; }
+	public String getLieu() { return lieu; }
+	
+	/*
+	 * Informations of the session
+	 */
+	public void setInfos(String infos) { this.infos = infos; }
+	public String getInfos() { return infos; }
+	
+	/*
+	 * Complete list of candidates for the session
+	 */
+	public void setCandidats(List<String> candidats) { this.candidats = candidats; }
+	public List<String> getCandidats() { return candidats; }
+	
+	/*
+	 * List of accepted candidates
+	 */
+	public void setAccepte(List<String> accepte) { this.accepte = accepte; }
+	public List<String> getAccepte() { return accepte; }
+	
+	/*
+	 * List of pending candidates
+	 */
+	public void setAttente(List<String> attente) { this.attente = attente; }
+	public List<String> getAttente() { return attente; }
+	
+	/*
+	 * List of refused candidates
+	 */
+	public void setRefuse(List<String> refuse) { this.refuse = refuse; }
+	public List<String> getRefuse() { return refuse; }
+
+
+}
